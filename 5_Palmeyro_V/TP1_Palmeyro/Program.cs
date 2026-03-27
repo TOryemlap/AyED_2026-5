0ea2176da64b34037856e311a2f847721ba4e0a7
TOryemlap <75763986+TOryemlap@users.noreply.github.com>
2026-03-27 15:53:52 -0300

    Add files via upload

---
+++ 5_Palmeyro_V/TP1_Palmeyro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_Palmeyro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String pelicula;
+
+            Console.WriteLine("Hola crack, maquina, bestia, etc...");
+            Console.Write("¿Cual es tu nombre?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+            Console.Write("Hola " + nombre + ". Tu pelicula preferida " + pelicula + " es una porqueria.");
+        }
+    }
+}
