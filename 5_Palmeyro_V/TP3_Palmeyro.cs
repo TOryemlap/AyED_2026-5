df93e2e1f0ab27287c2b9daeb4f77150589902a3
TOryemlap <75763986+TOryemlap@users.noreply.github.com>
2026-03-27 16:29:32 -0300

    Add files via upload

---
+++ 5_Palmeyro_V/TP3_Palmeyro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_Palmeyro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola usuario");
+            Console.Write("Introduzca su año de nacimiento: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+            int edad = 2026 - año;
+            Console.WriteLine("Tu edad si naciste en el año " + año + " es de " + edad + " años.");
+        }
+    }
+}
