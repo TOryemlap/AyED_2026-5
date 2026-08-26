800e145a2f79f8c7a2a06f074a0673eddf2e49d3
TOryemlap <75763986+TOryemlap@users.noreply.github.com>
2026-08-26 17:00:55 -0300

    Add files via upload

---
+++ 5_Palmeyro_V/funciones1/1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void ContarHasta(int numero)
+        {
+            for (int i = 1; i <= numero; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un numero para la cuenta regresiva: ");
+            int numero = int.Parse(Console.ReadLine());
+            ContarHasta(numero);
+        }
+    }
+}
