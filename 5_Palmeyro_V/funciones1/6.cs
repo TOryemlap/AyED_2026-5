970bbecea39c36cbe95ff22dd4539f02e65d1950
TOryemlap <75763986+TOryemlap@users.noreply.github.com>
2026-08-28 16:52:45 -0300

    Add files via upload

---
+++ 5_Palmeyro_V/funciones1/6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Primos(int num)
+        {
+            int div = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    div++;
+                }
+            }
+            if (div == 2)
+            {
+                Console.WriteLine("El numero es primo!!!!!!!!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es primo...........");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresé un numero para ver si es primo o no.");
+            int num = int.Parse(Console.ReadLine());
+            Primos(num);
+        }
+    }
+}
