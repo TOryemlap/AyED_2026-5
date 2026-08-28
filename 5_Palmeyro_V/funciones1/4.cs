7036daba4e2e68bf2605e5f7f9f30c6b2ae502de
TOryemlap <75763986+TOryemlap@users.noreply.github.com>
2026-08-28 15:42:49 -0300

    Add files via upload

---
+++ 5_Palmeyro_V/funciones1/4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Factorial(int numero)
+        {
+            int suma = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+                suma = suma + i;
+            }
+            return suma;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            int res = Factorial(numero);
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ 5_Palmeyro_V/funciones1/5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Potencia(int numero, int pot)
+        {
+            int suma = 1;
+            for (int i = 1; i <= pot; i++)
+            {
+                suma = suma * numero;
+            }
+            return suma;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba una potencia: ");
+            int pot = int.Parse(Console.ReadLine());
+            int res = Potencia(numero, pot);
+            Console.WriteLine(res);
+        }
+    }
+}
